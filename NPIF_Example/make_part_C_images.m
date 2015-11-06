9092dc1643833cab81512288b04e6e1c21ec6e85
btdevree <btdevree@gmail.com>
2015-11-06 09:38:56 -0500

    part C saves data and images

--- NPIF_Example/make_part_C_images.m
+++ NPIF_Example/make_part_C_images.m
@@ -8,8 +8,8 @@ binary_path = [binary_path_parts{1}, 'CLM_figures_and_data/'];
 
 % Define variable quantities for the S/N and event_number
 SN_ratios = [1; 4; 10];
-true_event_numbers = [1e3; 2e3; 5e3];% 1e4; 2e4; 5e4; 1e5; 2e5; 5e5; 1e6; 2e6; 5e6; 1e7];
-replicates = 5;
+true_event_numbers = [1e3; 2e3; 5e3; 1e4; 2e4; 5e4; 1e5; 2e5; 5e5; 1e6; 2e6; 5e6; 1e7];
+replicates = 3;
 
 % Generate seeds 
 num_ratios = size(SN_ratios, 1);
@@ -60,14 +60,23 @@ for SN_index = 1:size(SN_ratios, 1)
             seed = seeds(index); % Uses a linear index
             param_struct = param_array_group{index};
 
-            % Create data
+            % Create data and save it
             [data_ch1, data_ch2, ~, STORMvars] = create_test_data_dv(param_struct, seed);
+            ch1_datasets_group{index} = data_ch1;
+            ch2_datasets_group{index} = data_ch2;
 
             % Create image and save it in cell array
             [image, ~, ~] = create_test_STORM_images_dv(param_struct, data_ch1, data_ch2, STORMvars);
             images_array{index} = image;
     end
-   
+    
+    % Let user know about progress
+    disp(['finished S/N ratio = ', num2str(SN_ratio)]);
+    
+    % Copy the datasets into the main cell arrays
+    ch1_datasets(SN_index, :, :) = ch1_datasets_group;
+    ch2_datasets(SN_index, :, :) = ch2_datasets_group;
+        
     % Save the group of images
     save([binary_path, 'NPIF_part_C_images_SN', num2str(SN_ratio), '.mat'], 'images_array');
 end
