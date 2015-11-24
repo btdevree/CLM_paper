aadd3e67f76be7b37255d8d29634a25f16fa9f6d
btdevree <btdevree@gmail.com>
2015-11-24 00:35:42 -0500

    C++ progress

--- Common/Gauss_STORM_image_MEX.cpp
+++ Common/Gauss_STORM_image_MEX.cpp
@@ -1,3 +1,4 @@
+// Include needed libraries
 # include <math.h>
 # include <matrix.h>
 # include <mex.h>
@@ -29,8 +30,53 @@
  *      the center of each pixel, generated with meshgrid.
 */
 
+// Define the input and output names for convenience
+# define RESOLUTION_IN  prhs[0]
+# define XY_DATA_IN     prhs[1]
+# define COVAR_INV_IN   prhs[2]
+# define COVAR_DET_IN   prhs[3]
+# define CUTOFF_PX_X_IN prhs[4]
+# define CUTOFF_PX_Y_IN prhs[5]
+# define XMESH_IN       prhs[6]
+# define YMESH_IN       prhs[7]
+# define IMAGE_OUT      plhs[0]
+
 void mexFunction(int nlhs, mxArray *plhs[], 
                  int nrhs, const mxArray *prhs[])
 {
-    plhs[0] = mxCreateDoubleMatrix (10, 10, mxREAL);
+    // Define variables
+    int m, n, cutoff_x, cutoff_y;
+    mxArray* image_ptr;
+    double* image, xy_data, covar_inv, xmesh, ymesh;
+    double value, resolution, covar_det;
+    
+    //Assign array variables from pointers
+    xy_data = mxGetPr(XY_DATA_IN);
+    covar_inv = mxGetPr(COVAR_INV_IN);
+    xmesh = mxGetPr(XMESH_IN);
+    ymesh = mxGetPr(YMESH_IN);
+        
+    // Get the dimensions of the image 
+    m = mxGetM(XMESH_IN); 
+    n = mxGetN(XMESH_IN);
+        
+    /*
+    xmesh = mxGetPr(XMESH_IN); // Get pointer to xmesh
+    ymesh = mxGetPr(YMESH_IN); // Get pointer to ymesh
+    */
+    mexPrintf("Hello world\n");
+
+    image_ptr = mxCreateDoubleMatrix (m, n, mxREAL);
+    image = mxGetPr(image_ptr);
+    image[0] = 13;
+    value = ymesh[0];
+    
+    mexPrintf("%g\n", value);
+    
+    IMAGE_OUT = image_ptr;
+    
 }
+
+
+
+
