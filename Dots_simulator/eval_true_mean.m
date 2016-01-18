68dcf99a4ba1ad356cbe852d961a76c557447d11
btdevree <btdevree@gmail.com>
2016-01-18 17:31:11 -0500

    Switching over to parfeval instead of parfor, working on bootstrap section of run_dots_correlation

---
+++ Dots_simulator/eval_true_mean.m
@@ -0,0 +1,18 @@
+function [distance_vector, mean_vector] = eval_true_mean(params, number_dots, max_correlation_radius, dots_per_cell_mean,...
+    dot_radius, dot_correlation_value, label_density_mean, label_density_stdev, label_SN_ratio, event_overcounting,...
+    dot_center_precision, event_precision, STORM_pixel_resolution, method_type)
+%EVAL_TRUE_MEAN Funciton for parallel evaluation of a true-mean dots
+%simulation
+
+% Get a results stack
+[correlation_stack, ~] = collect_dot_correlations(params, number_dots, max_correlation_radius, dots_per_cell_mean,...
+    dot_radius, dot_correlation_value, label_density_mean, label_density_stdev, label_SN_ratio, event_overcounting,...
+    dot_center_precision, event_precision, STORM_pixel_resolution, method_type);
+
+% Average the stack
+correlation_image = mean(correlation_stack, 3);
+
+% Get a radial average
+[distance_vector, mean_vector, ~, ~] = radial_average_2D_correlation(correlation_image);
+end
+
--- Dots_simulator/run_dots_correlation.m
+++ Dots_simulator/run_dots_correlation.m
@@ -10,7 +10,9 @@ figure_name = 'stdev_vs_num_dots.png';
 load('dots_params.mat') % loads as 'params'
 
 % Set parameters
-number_dots_vector = [10, 20, 30, 50, 70, 100, 150, 200, 300, 400, 500];
+number_replicates = 5;
+number_bootstraps = 5;
+number_dots_vector = [10];%, 20, 30, 50, 70, 100, 150, 200, 300, 400, 500];
 max_correlation_radius = 1000; %nm
 dots_per_cell_mean = 25;
 dot_radius = 100; %nm
@@ -33,33 +35,92 @@ for number_dot_index = 1:length(number_dots_vector)
     correlation_width = 2*max_radius_px + 1;
 
     % Repeat dot collection and averageing 
-    number_replicates = 6;
-    mean_vector_stack = zeros(max_radius_px + 1, number_replicates);
-    for replicate_index = 1:number_replicates
+    for index = 1:number_replicates
 
-        % Get a results stack
-        [correlation_stack, number_cells] = collect_dot_correlations(params, number_dots, max_correlation_radius, dots_per_cell_mean,...
+        % Evaluate the function asynchronously
+        future_results(index) = parfeval(@eval_true_mean, 2, params, number_dots, max_correlation_radius, dots_per_cell_mean,...
             dot_radius, dot_correlation_value, label_density_mean, label_density_stdev, label_SN_ratio, event_overcounting,...
-            dot_center_precision, event_precision, STORM_pixel_resolution, 'pdf');
+            dot_center_precision, event_precision, STORM_pixel_resolution, 'pdf'); 
+    end
     
-        % Average the stack
-        correlation_image = mean(correlation_stack, 3);
+    % Collect results
+    mean_vector_stack = zeros(max_radius_px + 1, number_replicates);
+    for index = 1:number_replicates
 
-        % Get a radial average
-        [distance_vector, mean_vector, ~, ~] = radial_average_2D_correlation(correlation_image);
-
-        % Add to the results stack
-        mean_vector_stack(:, replicate_index) = mean_vector;
+        % Get next available result, fetchNext blocks until next results are available.
+        [completed_index, distance_vector, mean_vector] = fetchNext(future_results);
+      
+        % Save in appropreate slice
+        mean_vector_stack(:, completed_index) = mean_vector;
     end
     
     % Store the mean vector stack in a cell
     mean_vector_stack_cells{number_dot_index} = mean_vector_stack;
-    fprintf('finished number_dots = %d\n', number_dots);
+    fprintf('finished true mean number_dots = %d\n', number_dots);
 end
+
+mean_bootstrap_vector_stack_cells = cell(number_bootstraps, size(number_dots_vector, 2));
+% Repeat for each number of dots
+for number_dot_index = 1:length(number_dots_vector)
+    number_dots = number_dots_vector(number_dot_index);
+    
+    % Calculate the size of the correlation stack
+    max_radius_px = ceil(max_correlation_radius / STORM_pixel_resolution);
+    correlation_width = 2*max_radius_px + 1;
+    
+    % Repeat entire bootstrapping process in parallel
+    for index = 1:number_bootstraps
+
+        % Evaluate the function asynchronously
+        future_results(index) = parfeval(@eval_bootstrap_mean, 2, number_replicates, params, number_dots, max_correlation_radius, dots_per_cell_mean,...
+            dot_radius, dot_correlation_value, label_density_mean, label_density_stdev, label_SN_ratio, event_overcounting,...
+            dot_center_precision, event_precision, STORM_pixel_resolution, 'pdf'); 
+    end
+    
+    % Collect results
+    mean_vector_stack = zeros(max_radius_px + 1, number_replicates);
+    for index = 1:number_replicates
+
+        % Get next available result, fetchNext blocks until next results are available.
+        [completed_index, distance_vector, mean_vector] = fetchNext(future_results);
+      
+        % Save in appropreate slice
+        mean_vector_stack(:, completed_index) = mean_vector;
+    end
+    % Repeat entire bootstrapping process
+    for bootstrap_index = 1:number_bootstraps
+        
+        % Get a results stack
+        [correlation_stack, number_cells] = collect_dot_correlations(params, number_dots, max_correlation_radius, dots_per_cell_mean,...
+            dot_radius, dot_correlation_value, label_density_mean, label_density_stdev, label_SN_ratio, event_overcounting,...
+            dot_center_precision, event_precision, STORM_pixel_resolution, 'pdf');
+
+        % Repeat getting the mean with resampling
+        mean_vector_stack = zeros(max_radius_px + 1, number_replicates);
+        for replicate_index = 1:number_replicates
+
+            % Permute the stack
+            permuted_indices = randi(size(correlation_stack, 3));
+            permuted_stack = cat(3, correlation_stack(:, :, permuted_indices));
+            permuted_image = mean(permuted_stack, 3);
+
+            % Get a radial average
+            [distance_vector, mean_vector, ~, ~] = radial_average_2D_correlation(permuted_image);
+
+            % Add to the results stack
+            mean_vector_stack(:, replicate_index) = mean_vector;
+        end
+        
+        % Store the mean vector stack in a cell
+        mean_bootstrap_vector_stack_cells{bootstrap_index, number_dot_index} = mean_vector_stack;
+    end
+    fprintf('finished bootstrap number_dots = %d\n', number_dots);
+end
+
 
 % Save data
 save([figure_path, data_name], 'distance_vector', 'mean_vector_stack_cells', 'number_dots_vector', 'max_correlation_radius', 'dots_per_cell_mean', 'dot_radius',...
     'dot_correlation_value', 'label_density_mean', 'label_density_stdev', 'label_SN_ratio', 'event_overcounting', 'dot_center_precision',...
-    'event_precision', 'STORM_pixel_resolution');
+    'event_precision', 'STORM_pixel_resolution', 'mean_bootstrap_vector_stack_cells');
 
 
