725c6d39b001dada49c7ffc71b9667d0932c0fb6
btdevree <btdevree@gmail.com>
2016-05-06 17:17:50 -0400

    test changes to calculate_discrepency next

--- Common/calculate_discrepency.m
+++ Common/calculate_discrepency.m
@@ -1,4 +1,4 @@
-function [ discrepency_value ] = calculate_discrepency( experimental_image, ideal_image, method, NVI_entropy_bins)
+function [ discrepency_value, optimized_scale ] = calculate_discrepency( experimental_image, ideal_image, method, optimize_scaling, NVI_entropy_bins)
 %CALCULATE_DISCREPENCY Calculate a discrepency measure of the experimental
 %   image to the ideal image according the the chosen method.
 %
@@ -18,15 +18,22 @@ function [ discrepency_value ] = calculate_discrepency( experimental_image, idea
 %       'normalized_variation_of_information': 1 - variation of information
 %           (i.e. the mutual information normalized by the joint entropy 
 %           calculated from intensity histograms.
+%   optimize_scaling: boolean, if true the experimental image is scaled in
+%       order to minimize the returned discrepency value. Default = true.
 %   NVI_entropy_bins: number of bins to use in for the normalized variation
 %       of information method. Integer, ignored for other methods. 
 %       Default = 100
 % Outputs:
 %   discrepency_value: floating-point double, value of discrpency between 
 %       the experimental and ideal image.
+%   optimized_scale: optimized scaling factor for the experimental image
 
 % Set defaults
-if nargin < 4; NVI_entropy_bins = 100; end;
+if nargin < 4; optimize_scaling = true;
+if nargin < 5; NVI_entropy_bins = 100; end;
+
+% Start with optimized scale = 1
+optimized_scale = 1;
 
 % Convert to full matrices, if needed
 if issparse(experimental_image)
@@ -52,54 +59,84 @@ if ideal_max ~= 0 % Don't divide by zero!
     ideal_image = ideal_image / ideal_max;
 end
 
+% Optimize scale_factor, if requested
+if optimize_scaling
+    
+    % Calculate the discrepency as the sum of squared distance
+    if strcmp(method, 'sum_of_squares')
+        % Optimize with ssq
+        f = @(scale_factor)calc_ssq(scale_factor, ideal_image, experimental_image);
+    end
+
+    % Calculate the discrepency as the sum of l2 distance
+    if strcmp(method, 'l2_norm')
+        % Optimize with ssq
+        f = @(scale_factor)calc_l2_norm(scale_factor, ideal_image, experimental_image);
+    end
+
+    % Calculate the discrepency as the sum of the discrimination
+    if strcmp(method, 'discrimination')
+        % Calc experimental value
+        f = @(scale_factor)calc_discrim(scale_factor, ideal_image, experimental_image);
+    end
+    
+    % Calculate the discrepency with mutual information based Normalized Variation of Information
+    if strcmp(method, 'normalized_variation_of_information')
+        % Calc discrepency
+        f = @(scale_factor)calc_NVI(scale_factor, ideal_image, experimental_image, '2', NVI_entropy_bins);
+    end
+    
+    % Run the optimization
+    [optimized_scale] = fminunc(f, 1);    
+end
+
+% Get the discrepency for return
 % Calculate the discrepency as the sum of squared distance
 if strcmp(method, 'sum_of_squares')
-   
-    % Calc experimental ssq value
-    squares = (ideal_image - experimental_image).^2;
-    discrepency_value = sum(squares(:));
-    
+    [discrepency_value] = calc_ssq(scale_factor, ideal_image, experimental_image);
 end
 
 % Calculate the discrepency as the sum of l2 distance
 if strcmp(method, 'l2_norm')
-   
-    % Calc experimental l2 norm sum value
-    squares = (ideal_image - experimental_image).^2;
-    discrepency_value = sum(sqrt(squares(:)));
-    
+    [discrepency_value] = calc_l2_norm(scale_factor, ideal_image, experimental_image);
 end
 
 % Calculate the discrepency as the sum of the discrimination
 if strcmp(method, 'discrimination')
-   
-    % Calc experimental value
-    discrepency_value = calc_discrim(ideal_image, experimental_image);
-    
+    [discrepency_value] = calc_discrim(scale_factor, ideal_image, experimental_image);
 end
 
 % Calculate the discrepency with mutual information based Normalized Variation of Information
 if strcmp(method, 'normalized_variation_of_information')
-    
-    % Calc discrepency
-    discrepency_value = calc_NVI(ideal_image, experimental_image, '2', NVI_entropy_bins);
-  
+    [discrepency_value] = calc_NVI(scale_factor, ideal_image, experimental_image, '2', NVI_entropy_bins);
 end
 end
 
 % Local function to calculate the discrimination
-function [value] = calc_discrim(A, B)
-    all_values = A .* log2(A ./ B) - A + B;
+function [value] = calc_discrim(scale_factor, A, B)
+    sB = scale_factor .* B;
+    all_values = A .* log2(A ./ sB) - A + sB;
     value = sum(all_values(:));
 end
 
 % Local function to calculate the NVI
-function [NVI] = calc_NVI(A, B, log_base, number_bins)
+function [NVI] = calc_NVI(scale_factor, A, B, log_base, number_bins)
+    sB = scale_factor .* B;
     H_A = calc_image_entropy(A, log_base, number_bins);
-    H_B = calc_image_entropy(B, log_base, number_bins);
-    H_AB = calc_image_joint_entropy(A, B, log_base, number_bins);
+    H_B = calc_image_entropy(sB, log_base, number_bins);
+    H_AB = calc_image_joint_entropy(A, sB, log_base, number_bins);
     I_AB = H_A + H_B - H_AB;
     NVI = 1 - (I_AB/H_AB); % Bounded between 1 and 0; 0 = perfect identity
 end
 
+% Local funciton to calculate the SSQ error
+function [error] = calc_ssq(scale_factor, A, B)
+    squares = (A - scale_factor .* B).^2;
+    error = sum(squares(:));
+end
 
+% Local funciton to calculate the L2 norm error
+function [error] = calc_l2_norm(scale_factor, A, B)
+    squares = (A - scale_factor .* B).^2;
+    error = sum(sqrt(squares(:)));
+end
--- Fractional_discrepency/plot_IIC_curves.m
+++ Fractional_discrepency/plot_IIC_curves.m
@@ -10,14 +10,34 @@ method_name = 'SSQ';
 [approx_FD_mean, approx_FD_stdev] = calc_mean_stdev_FD_matrix(approx_SSQ_results);
 [ideal_FD_mean, ideal_FD_stdev] = calc_mean_stdev_FD_matrix(ideal_SSQ_results);
 
-% Calculate responce curves
+% Calculate information improvement curves
+% Get max and min discrepency values
 approx_max_discrepency = approx_FD_mean(end, :);
 approx_min_discrepency = approx_FD_mean(1, :);
+
 ideal_max_discrepency = ideal_FD_mean(end, :);
 ideal_min_discrepency = ideal_FD_mean(1, :);
 
+% Transform to fractional improvement of the discrepency 
 approx_delta_discrepency = approx_FD_mean - repmat(approx_min_discrepency, size(approx_FD_mean, 1), 1);
-approx_max_delta
+approx_max_delta = approx_max_discrepency - approx_min_discrepency;
+approx_info_improvement = 1-(approx_delta_discrepency ./ repmat(approx_max_delta, size(approx_FD_mean, 1), 1));
+
+ideal_delta_discrepency = ideal_FD_mean - repmat(ideal_min_discrepency, size(ideal_FD_mean, 1), 1);
+ideal_max_delta = ideal_max_discrepency - ideal_min_discrepency;
+ideal_info_improvement = 1-(ideal_delta_discrepency ./ repmat(ideal_max_delta, size(ideal_FD_mean, 1), 1));
+
+% Calculate AUC and TCI/ICI/ECI
+dFrac = event_fractions(1:end-1) - event_fractions(2:end);
+midpoint_II_approx = (approx_info_improvement(2:end, :) + approx_info_improvement(1:end-1, :))/2;
+AUC_approx = sum(repmat(dFrac, 1, size(midpoint_II_approx, 2)) .* midpoint_II_approx, 1);
+ECI = (2*AUC_approx - 1)';
+
+midpoint_II_ideal = (ideal_info_improvement(2:end, :) + ideal_info_improvement(1:end-1, :))/2;
+AUC_ideal = sum(repmat(dFrac, 1, size(midpoint_II_ideal, 2)) .* midpoint_II_ideal, 1);
+ICI = (2*AUC_ideal - 1)';
+
+TCI = (1 - (ideal_min_discrepency ./ ideal_max_discrepency))';
 
 % Get figure path
 figure_path_parts = strsplit(pwd, 'CLM_paper');
@@ -28,48 +48,36 @@ figure
 hold on
 set(gcf, 'Position', [100, 100, 800, 600]);
 set(gcf, 'PaperPositionMode', 'auto');
-ideal_zeros = ideal_FD_mean(end, :);
-plot(repmat(event_fractions, 1, size(ideal_FD_mean, 2)), ideal_FD_mean./repmat(ideal_zeros, size(ideal_FD_mean, 1), 1));
+plot(repmat(event_fractions, 1, size(ideal_FD_mean, 2)), ideal_info_improvement);
 xlabel('Fraction of events included')
-ylabel('Relative Sum of Squares')
-title('Old SSQ ideal relative fractional discrepency');
+ylabel('SSQ fractional information improvement')
+title('SSQ Ideal Information Improvement Charactistic curve');
 hold off
-print([figure_path, 'Old_Ideal_relative_FD_SN', num2str(SN_ratio), '_', method_name, '.png'], '-dpng');
+print([figure_path, 'Ideal_IIC_SN', num2str(SN_ratio), '_', method_name, '.png'], '-dpng');
 delete(gcf)
 
 figure
 hold on
 set(gcf, 'Position', [100, 100, 800, 600]);
 set(gcf, 'PaperPositionMode', 'auto');
-approx_zeros = approx_FD_mean(end, :);
-plot(repmat(event_fractions, 1, size(approx_FD_mean, 2)), approx_FD_mean./repmat(approx_zeros, size(approx_FD_mean, 1), 1));
+plot(repmat(event_fractions, 1, size(approx_FD_mean, 2)), approx_info_improvement);
 xlabel('Fraction of events included')
-ylabel('Relative Sum of Squares')
-title('Old SSQ approximated relative fractional discrepency');
+ylabel('SSQ fractional information improvement')
+title('SSQ Approximated Information Improvement Charactistic curve');
 hold off
-print([figure_path, 'Old_Approx_relative_FD_SN', num2str(SN_ratio), '_', method_name, '.png'], '-dpng');
+print([figure_path, 'Approx_IIC_SN', num2str(SN_ratio), '_', method_name, '.png'], '-dpng');
 delete(gcf)
 
 figure
 hold on
 set(gcf, 'Position', [100, 100, 800, 600]);
 set(gcf, 'PaperPositionMode', 'auto');
-plot(repmat(event_fractions, 1, size(ideal_FD_mean, 2)), ideal_FD_mean);
-xlabel('Fraction of events included')
-ylabel('Sum of Squares')
-title('Old SSQ ideal fractional discrepency');
+plot(repmat(true_event_numbers, 1, 3), [TCI, ICI, ECI]);
+xlabel('Number of events in image')
+ylabel('Ideal or Estimated Completeness Index')
+title('Ideal or Estimated Completeness Index vs. event number');
+legend('TCI', 'ICI', 'ECI', 'Location', 'southeast');
+set(gca,'XScale','log');
 hold off
-print([figure_path, 'Old_Ideal_raw_FD_SN', num2str(SN_ratio), '_', method_name, '.png'], '-dpng');
-delete(gcf)
-
-figure
-hold on
-set(gcf, 'Position', [100, 100, 800, 600]);
-set(gcf, 'PaperPositionMode', 'auto');
-plot(repmat(event_fractions, 1, size(approx_FD_mean, 2)), approx_FD_mean);
-xlabel('Fraction of events included')
-ylabel('Sum of Squares')
-title('Old SSQ approximated fractional discrepency');
-hold off
-print([figure_path, 'Old_Approx_raw_FD_SN', num2str(SN_ratio), '_', method_name, '.png'], '-dpng');
+print([figure_path, 'CI_SN', num2str(SN_ratio), '_', method_name, '.png'], '-dpng');
 delete(gcf)
