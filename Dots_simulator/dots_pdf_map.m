cad680cb72e4ba19b89cd53728cdf979fe813570
btdevree <btdevree@gmail.com>
2016-06-20 15:51:29 -0400

    Working on polygon region images, renamed dots function, removed cell shape from dots

--- Dots_simulator/dots_pdf_map.m
+++ Dots_simulator/dots_pdf_map.m
@@ -1,14 +1,13 @@
-function [ pdf_map, center_coords, STORM_mask] = dots_pdf_map(number_of_dots, dot_radius, dot_to_background_ratio, parameter_struct)
+function [ pdf_map, center_coords, STORM_mask] = dots_pdf_map(parameter_struct, number_of_dots, dot_radius, dot_to_background_ratio)
 %DOTS_PDF_MAP Makes a pdf map of circular regions with uniform density 
-%   inside the main cell density of a simulated movie dataset.
-%
-%   Each dot is randomly placed within the boundries of the simulated cell. 
+%   randomly distributed in an image.
 %
 % Inputs:
 %   number_of_dots: number of regions to put in the pdf map.
 %   dot_radius: radius of each circular region.
-%   dot_to_background_ratio: ratio of the density of events inside the dot
-%       region to the general cellular background
+%   dot_to_background_ratio: ratio of the density of events inside the
+%       dot region to the general background. Use Inf for a image with no 
+%       background.
 %   parameter_struct: parameter structure made with
 %       test_movie_parameters_dv.
 % Outputs:
@@ -33,53 +32,24 @@ max_y_bound = params.bounds(4);
 x_length = max_x_bound - min_x_bound;
 y_length = max_y_bound - min_y_bound;
 
-% Determine cell center
-if strcmp(params.cell_center, 'centered')
-    cell_center_x = (x_length)/2;
-    cell_center_y = (y_length)/2;
-else
-    cell_center_x = params.cell_center(1);
-    cell_center_y = params.cell_center(2);
-end
-cell_center = [cell_center_x, cell_center_y];
-
-% Determine cell radius
-if isnumeric(params.cell_radius)
-    cell_radius = params.cell_radius;
-else
-    tokens = regexp(params.cell_radius, '(\d+)([%])', 'tokens'); % Look for a percentage value
-    if ~isempty(tokens)
-        max_radius = min([max_x_bound - cell_center_x, cell_center_x - min_x_bound,...
-            max_y_bound - cell_center_y, cell_center_y - min_y_bound]);
-        cell_radius = (str2num(tokens{1}{1})/100) * max_radius;
-    end
-end
-
-% Keep all the pdf density inside the cell
-cell_radius_for_dots = cell_radius - dot_radius;
-if cell_radius_for_dots <= 0
-    error('Cell is not large enough to hold circular regions of requested size');
-end
-
-% Initialize variables
-center_coords = zeros(number_of_dots, 2);
-event_counter = 0;
-
-% Repeat loop until enough random events inside the circle are generated
-while event_counter < number_of_dots 
-    event_coord = (rand(1,2) * cell_radius_for_dots * 2) - cell_radius_for_dots;
-    if sqrt(sum(event_coord.^2)) <= cell_radius_for_dots
-       event_counter = event_counter + 1;
-       center_coords(event_counter, :) = event_coord + cell_center;
-    end
-end
-
-% Get the cell map
+%Calc needed number of pixels
 map_resolution = params.ch1_distribution_params{2};
-[pdf_map, xmesh, ymesh] = create_cell_map(map_resolution, x_length, y_length, cell_center_x, cell_center_y, cell_radius);
+num_pixels_x = ceil(x_length/map_resolution);
+num_pixels_y = ceil(y_length/map_resolution);
+
+% Calc meshgrid
+x_vec = [0.5: 1: num_pixels_x - 0.5] .* map_resolution;
+y_vec = [num_pixels_y - 0.5: -1: 0.5] .* map_resolution;
+[xmesh, ymesh] = meshgrid(x_vec, y_vec);
+
+% Initalize pdf_map and center point matricies
+pdf_map = zeros(size(xmesh));
+center_coords_x = rand(number_of_dots, 1) * (x_length - 2 * dot_radius) + dot_radius;
+center_coords_y = rand(number_of_dots, 1) * (y_length - 2 * dot_radius) + dot_radius;
+center_coords = [center_coords_x, center_coords_y];
 
 % Multiply all pixels inside spot radius by deisred dot to background ratio
-for dot_index = 1:size(center_coords, 1)
+for dot_index = 1:number_of_dots
     center_x = center_coords(dot_index, 1);
     center_y = center_coords(dot_index, 2);
     
@@ -89,44 +59,14 @@ for dot_index = 1:size(center_coords, 1)
     delta_dist = sqrt(delta_x.^2 + delta_y.^2);
     
     % Add 1 to the pdf map for the pixels that are within the radius
-    factors = ones(size(xmesh));
-    factors(delta_dist <= dot_radius) = dot_to_background_ratio;
-    pdf_map = pdf_map .* factors;
+    pdf_map = pdf_map + double(delta_dist <= dot_radius); 
 end
 
 % Normalize the pdf map
+if ~isinf(dot_to_background_ratio) % Any non-perfect image
+    pdf_map = pdf_map * dot_to_background_ratio; % Multiply by ratio
+    pdf_map = pdf_map + ones(size(pdf_map)); % Add one for background
+end
 map_sum = sum(pdf_map(:));
 pdf_map = pdf_map / map_sum;
-
-% Copy to the image mask if requested - Move this to another funciton in
-% the next refactor, it doesn't really fit that well here. !!!!!!! Replace
-% with
-% create_test_cell_STORM_mask!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-if nargout == 3
-    STORM_mask = create_cell_map(params.STORM_pixel_size, x_length, y_length, cell_center_x, cell_center_y, cell_radius);
-end
-
-end
-
-function [cell_map, xmesh, ymesh] = create_cell_map(map_resolution, x_length, y_length, cell_center_x, cell_center_y, cell_radius)
-% Local function that returns a double matrix with a circular region drawn 
-%   in the cell region and the pixel coordinate meshgrid matrices.
-
-%Calc needed number of pixels
-num_pixels_x = ceil(x_length/map_resolution);
-num_pixels_y = ceil(y_length/map_resolution);
-
-% Calc meshgrid
-x_vec = [0.5: 1: num_pixels_x - 0.5] .* map_resolution;
-y_vec = [num_pixels_y - 0.5: -1: 0.5] .* map_resolution;
-[xmesh, ymesh] = meshgrid(x_vec, y_vec);
-
-% Calc delta distances from center
-delta_x = xmesh - cell_center_x;
-delta_y = ymesh - cell_center_y;
-delta_dist = sqrt(delta_x.^2 + delta_y.^2);
-
-% Returun image with 1 as for the pixels that are within the cell radius and 0 for those outside.
-dist_bools = delta_dist <= cell_radius;
-cell_map = double(dist_bools); 
 end
--- Region_simulator/region_pdf_map.m
+++ Region_simulator/region_pdf_map.m
@@ -1,8 +1,8 @@
-function [pdf_map, control_points_x, control_points_y] = lines_pdf_map(parameter_struct, number_of_lines, line_width, line_to_background_ratio, line_type, chord_min_length, chord_max_length)
-%LINES_PDF_MAP Makes a pdf map of line structures in the field of
-% view.
+function [pdf_map, polygon_points] = region_pdf_map(parameter_struct, number_of_vertices, region_to_background_ratio)
+%REGION_PDF_MAP Makes a pdf map of a randomized polygon in then center of
+% the image.
 %
-% Each line is randomly placed within the image, assuming a Carteisain
+% A polygon with randomized verterx positions. Assumes a Carteisain
 % coordinate system. The origin is assumed to be in the lower left corner
 % of the lower left pixel unless the bounds indicate otherwise in the 
 % parameter structure.
@@ -10,33 +10,16 @@ function [pdf_map, control_points_x, control_points_y] = lines_pdf_map(parameter
 % Inputs:
 %   parameter_struct: parameter structure made with
 %       test_movie_parameters_dv.
-%   number_of_lines: integer, number of lines to put in the pdf map.
-%   line_width: width of the lines, in nanometers.
-%   line_to_background_ratio: ratio of the density of events inside the
-%       line region to the general background. Use Inf for a image with no 
-%       background.
-%   line_type: string, type of lines to draw. Options are:
-%       'line_segment' - straight line segments
-%       'quadratic' - quadratic bezier (3 control points)
-%       'cubic' - cubic bezier (4 control points)
-%   chord_min_length: minimum length of the line chord in the image, given 
-%       in nanometers or as a string in the format 'xxx%' representing the 
-%       percentage of the smallest image dimenstion. Optional, default =
-%       '5%'.
-%   chord_max_length: maximum length of the line chord in the image, given 
-%       in nanometers or as a string in the format 'xxx%' representing the 
-%       percentage of the smallest image dimenstion. Optional, default =
-%       '95%'.
+%   number_of_vertices: integer, number of vertices to create the polygon
+%       with.
+%   region_to_background_ratio: ratio of the density of events inside the
+%       polygon region to the general background. Use Inf for a image with 
+%       no background.
 % Outputs:
 %   pdf_map: array of floating-point doubles, normalized sampling of the
 %       analytical pdf.
-%   control_points_x/_y: coordinates of the control points for each bezier 
-%       curve in the image. Each row is a new curve, and the columns are 2
-%       to 4 entrys longe, depending on the line_type.
-
-% Set defaults
-if nargin < 5; chord_min_length = '5%'; end;
-if nargin < 6; chord_max_length = '95%'; end;
+%   polygon_points: n by 2 matrix of (x, y) coordinates of the vertex 
+%       points of the polygon region.
 
 % Rename parameter structure for convenience
 params = parameter_struct;
@@ -49,28 +32,10 @@ max_y_bound = params.bounds(4);
 x_length = max_x_bound - min_x_bound;
 y_length = max_y_bound - min_y_bound;
 
-% Determine chord minimum length
-if ~isnumeric(chord_min_length)
-    tokens = regexp(chord_min_length, '(\d+)([%])', 'tokens'); % Look for a percentage value
-    if ~isempty(tokens)
-        base_length = min([x_length, y_length]);
-        chord_min_length = (str2num(tokens{1}{1})/100) * base_length;
-    end
-end
-
-% Determine chord max lengths
-if ~isnumeric(chord_max_length)
-    tokens = regexp(chord_max_length, '(\d+)([%])', 'tokens'); % Look for a percentage value
-    if ~isempty(tokens)
-        base_length = min([x_length, y_length]);
-        chord_max_length = (str2num(tokens{1}{1})/100) * base_length;
-    end
-end
-
-% Double check that min and max are not mixed up
-if chord_min_length > chord_max_length
-    error('Minimum line length must be smaller or equal to the maximum length');
-end
+% Determine the angle for each vertex and size of polygon
+base_angle = 2 * pi / number_of_vertices;
+max_dist_from_center = min(0.9 * x_length, 0.9 * y_length);
+max_base_radius = max_dist_from_center / 1.5; % 
 
 % Generate line coordinates, work in coordinates relative to lower left corner
 % Initialize variables
@@ -112,8 +77,18 @@ x_vec = [0.5: 1: num_pixels_x - 0.5] .* map_resolution;
 y_vec = [num_pixels_y - 0.5: -1: 0.5] .* map_resolution;
 [xmesh, ymesh] = meshgrid(x_vec, y_vec);
 
-% Initalize pdf map image
+% Initalize pdf_map and control point matricies
 pdf_map = zeros(size(xmesh));
+if strcmp(line_type, 'line_segment')
+    control_points_x = zeros(number_of_lines, 2);
+    control_points_y = zeros(number_of_lines, 2);
+elseif strcmp(line_type, 'quadratic')
+    control_points_x = zeros(number_of_lines, 3);
+    control_points_y = zeros(number_of_lines, 3);
+elseif strcmp(line_type, 'cubic')
+    control_points_x = zeros(number_of_lines, 4);
+    control_points_y = zeros(number_of_lines, 4);
+end
 
 % Calculate each line seperately and add to total map
 for line_index = 1:size(start_coords, 1)
@@ -122,20 +97,24 @@ for line_index = 1:size(start_coords, 1)
     
     % Prepare the control points for each type of line
     if strcmp(line_type, 'line_segment')
-        control_points = [start_coords(line_index, :); end_coords(line_index, :)];
+        cp = [start_coords(line_index, :); end_coords(line_index, :)];
     elseif strcmp(line_type, 'quadratic')
-        control_points = [start_coords(line_index, :); cp1_coords(line_index, :); end_coords(line_index, :)];
+        cp = [start_coords(line_index, :); cp1_coords(line_index, :); end_coords(line_index, :)];
     elseif strcmp(line_type, 'cubic')
-        control_points = [start_coords(line_index, :); cp1_coords(line_index, :); cp2_coords(line_index, :); end_coords(line_index, :)];
+        cp = [start_coords(line_index, :); cp1_coords(line_index, :); cp2_coords(line_index, :); end_coords(line_index, :)];
     end
     
+    % Record control points
+    control_points_x(line_index, :) = cp(:, 1)';
+    control_points_y(line_index, :) = cp(:, 2)';
+    
     % Approximate the arc length
-    testpoints = calc_bezier_line(control_points, 25); % 25 points should most always us a length within 1% of the true value for cubic beziers
+    testpoints = calc_bezier_line(cp, 25); % 25 points should most always us a length within 1% of the true value for cubic beziers
     approx_length = sum(sqrt(sum((testpoints(2:end, :) - testpoints(1:end-1, :)).^2 , 2)), 1);
     
     % Get approreate number of points along the bezier curve 
     number_curve_points = 5 * (approx_length / map_resolution); % about 1/10th pixel maximum errors on distance measurements
-    curve_points = calc_bezier_line(control_points, number_curve_points);
+    curve_points = calc_bezier_line(cp, number_curve_points);
     
     % Select pixels next to the line, too inaccurate to use as distance measurements but avoids calculating many unused distances
     [coord_linear_indices, x_coords, y_coords] = select_bezier_region(curve_points, xmesh, ymesh, line_width/2);
