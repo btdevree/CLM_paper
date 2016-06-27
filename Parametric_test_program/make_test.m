e9c41963bda7543e73c278ec46e6408ffb405e5b
btdevree <btdevree@gmail.com>
2016-06-27 13:53:03 -0400

    working on test prep

--- Parametric_test_program/make_test.m
+++ Parametric_test_program/make_test.m
@@ -1,4 +1,4 @@
-function make_test(output_directory, test_number)
+function make_test( parmeter_structure, output_directory, test_number)
 %MAKE_TEST Prepare a parametric STORM image test and save it into the 
 % specified directory.
 %
@@ -6,7 +6,9 @@ function make_test(output_directory, test_number)
 % Dots_simulator, and Region_simulator folders; make sure these are on the
 % MATLAB path. 
 %   
-% Input: 
+% Input:
+%   parameter_struct: parameter structure made with
+%       test_movie_parameters_dv.
 %   output_directory: string, filepath for saving the test folder
 %   test_number; integer, the number to assign to the test
 % Output:
@@ -16,10 +18,44 @@ function make_test(output_directory, test_number)
 %   test and a file "test_files.mat" with the information needed by the
 %   parametric STORM image testing GUI. 
 
+% Shuffle the RNG
+rng('shuffle');
+
 % Define test characteristics
 test_version = '0.1';
-actin_TCI_range = [
 
+% Regions
+region_number_images = 6;
+region_SNratios = [1, 4, 10];
+region_event_number_range = [3e2, 1e6];
+region_number_points = [6];
+
+% Dots
+dots_number_images = 15;
+dots_SNratios = [1, 4, 10];
+dots_sizes = [20, 50, 100, 200, 500]; 
+dots_event_number_range = [3e2, 3e6];
+dots_number_dots = [10];
+
+% Actin lines
+actin_number_images = 18;
+actin_SNratios = [1, 4, 10];
+actin_event_number_range = [3e2, 1e7];
+actin_line_types = {'line_segment', 'cubic', 'quadratic'};
+actin_line_width = [9, 26]; % actin and microtubules, respectivly
+
+% Border
+border_number_images = 15;
+border_SNratios = [1, 4, 10];
+border_event_number_range = [1e3, 1e7];
+border_roughness = [.35, .45, .55, .65, .75];
+border_displacement_factor = [.4, .35, .3, .25, .2]; % Apply together with the above roughness factor, not for all combinations possible
+
+% Add up number of images
+total_number_images = region_number_images + dots_number_images + actin_number_images + border_number_images;
+
+% Rename parameter structure for convenience
+params = parameter_struct;
 
 % Create folder
 mkdir(output_directory, ['Test_', num2str(test_number)]);
@@ -28,7 +64,8 @@ mkdir(output_directory, ['Test_', num2str(test_number)]);
 test_info = struct();
 test_info.test_number = test_number;
 test_info.test_version = test_version;
-test_info.image_info = cell(0);
+test_info.image_info = cell(total_number_images, 1);
+
 
 end
 
