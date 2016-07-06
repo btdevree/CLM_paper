2869d8c6be0f3edbe3eeb2caef381eb87fe79c10
btdevree <btdevree@gmail.com>
2016-07-06 11:43:53 -0400

    started analysis program

---
+++ Parametric_test_program/analyze_test.m
@@ -0,0 +1,35 @@
+function analyze_test(test_directory, output_directory)
+%ANALYZE_TEST Compare responses of a parametric STORM image test to the 
+% ground truth and save the summary statistics.
+%
+% Requires functions from Common, Actin_simulator, Border_simulator,
+% Dots_simulator, and Region_simulator folders; make sure these are on the
+% MATLAB path. 
+%   
+% Input:
+%   test_directory: string, filepath for the test folder where the test
+%       archive and response are kept. Optional, default = current 
+%       directory.
+%   output_directory: string, filepath for saving the test results.
+%       Optional, default = test_directory.
+% Output:
+%   Summary is saved to the output directory as "test_summary.mat"
+
+% Set defaults
+if nargin < 1; test_directory = ''; end;
+if nargin < 2; output_directory = test_directory; end;    
+
+% Start up a new parallel pool using default settings, if needed.
+gcp
+
+% Expand and load the test, load the answers
+untar([test_directory, '/test_archive.tar.gz'], output_directory);
+load([test_directory, '/test_archive.mat']); % Loads test_info
+load([test_directory, '/response_info.mat']); % Load responses
+
+% Initalize variables
+test_summary = struct();
+
+
+
+end
