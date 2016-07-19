94ad8390f39b079b6c1608490013f019ce1ce556
btdevree <btdevree@gmail.com>
2016-07-19 01:28:55 -0400

    more fixes to fractal dimension, still a overall distance bug that's directly related to start index

--- Common/calc_fractal_dimension.m
+++ Common/calc_fractal_dimension.m
@@ -23,11 +23,13 @@ if nargin < 4; show_Richardson_plot = false; end;
 % Set parameters and get constants
 number_divider_lengths = 7;
 total_number_points = size(line_points, 1);
+displacement = sqrt(sum((line_points(1, :) - line_points(end-1, :)).^2, 2));
 distances = sqrt(sum((line_points(2:end, :) - line_points(1:end-1, :)).^2, 2));
 maximum_length = sum(distances, 1);
 stdev_length = std(distances, 0, 1);
 skew_length = skewness(distances, 1, 1);
 mean_length = maximum_length / total_number_points;
+path_to_displacement_ratio = maximum_length / displacement;
 
 % Determine divider sizes
 if isempty(divider_length_range)
@@ -57,7 +59,7 @@ for repeat_index = 1:number_repeats
     for divider_index = 1:number_divider_lengths
         
         % Calculate the number of points that we should calculate the distance from for each cycle of the divider algorithm
-        num_points = calc_number_poins_per_divider(divider_lengths(divider_index), mean_length, stdev_length, skew_length);
+        num_points = calc_number_poins_per_divider(divider_lengths(divider_index), mean_length, stdev_length, skew_length, path_to_displacement_ratio);
         
         % Record arguments
         args(divider_index, repeat_index).divider_length = divider_lengths(divider_index);
@@ -68,7 +70,7 @@ end
 
 % % Nonparallel execution - Run instead of parallel feval for debuging/graphing
 % for eval_index = 1:size(args(:), 1)
-%     curve_distance(eval_index) = calc_distance(line_points, args(eval_index));
+%     curve_distance(eval_index) = calc_distance(line_points, args(eval_index), true);
 % end
 
 % Start up a pool of future calc_distance evaluations
@@ -96,10 +98,15 @@ fractal_dim = 1 - fit_parameters(2);
 % Show the plot if requested
 if show_Richardson_plot
     
+    %colors_mat = repmat(linspace(0, 1, number_repeats), size(divider_lengths, 1), 1);
+    colors_mat = vertcat(args.start_index);
+    %colors = colors_mat(:);
+    colors = colors_mat / max(colors_mat);
+    
     % Calc line to display
     display_line_x = linspace(log(min_divider), log(max_divider));
     display_line_y = fit_parameters(2) .* display_line_x + fit_parameters(1);
-    scatter(log_divider_matrix(:, 2), log_distance_matrix)
+    scatter(log_divider_matrix(:, 2), log_distance_matrix, 20, colors)
     hold on
     plot(display_line_x, display_line_y, 'r');
     xlabel('ln(divider length)')
@@ -107,17 +114,12 @@ if show_Richardson_plot
 end
 end
         
-function [distance, divider_points] = calc_distance(line_points, args)
+function [distance, divider_points] = calc_distance(line_points, args, draw_plots)
 % Local function to calculate the distance of a curve for a particular
 % divider length. Optional output gives all the divider points.
 
-% Unpack args
-divider_length = args.divider_length;
-start_index = args.start_index;
-points_required = args.points_required_per_divider; 
-
-% Enable/disable graphical feedback, false = off
-draw_plots = false;
+% Default is no plotting
+if nargin < 3; draw_plots = false; end;
 
 % See if we'll need to log points
 if nargout > 1
@@ -131,6 +133,11 @@ if draw_plots
     figure('Units', 'pixels', 'Position', [50, 50, 1000, 1000], 'PaperPositionMode', 'auto', 'InvertHardcopy', 'off');
     log_points_flag = true;
 end
+
+% Unpack args
+divider_length = args.divider_length;
+start_index = args.start_index;
+points_required = args.points_required_per_divider; 
 
 % Get starting coordinates
 start_coords = line_points(start_index, :);
@@ -406,7 +413,7 @@ elseif p1_OK_flag && p2_OK_flag;
 end
 end
 
-function [number_points] = calc_number_poins_per_divider(divider, mean, stdev, skew)
+function [number_points] = calc_number_poins_per_divider(divider, mean, stdev, skew, path_length_to_displacement_ratio)
 % Local function to estimate the number of points we'll need to assay in
 % order to include the next divider length 99.5% of the time. Accounts for
 % skewed step size distributions and small numbers of steps. 
@@ -424,18 +431,22 @@ while true
     path_skew = (1/sqrt(number_points)) * skew; % From skew definition, given in Eriksson, "A simulation method for skewness correction" (Master's thesis, Uppasla U.), Appendex 1, Corollary 4. 
     
     % Approximate as normal with a corrected mean due to the skew
-    adjusted_path_mean = path_mean + (path_skew / (6 * path_stdev.^2 * number_points)); % Norman J. Johnson, "Modified t Tests and Confidence Intervals for Asymmetrical Populations" JASA, v73:p536-44, eqn 2.7
+    adjusted_path_mean = path_mean - (path_skew / (6 * path_stdev.^2 * number_points)); % Norman J. Johnson, "Modified t Tests and Confidence Intervals for Asymmetrical Populations" JASA, v73:p536-44, eqn 2.7, but I swear the plus sign should be a minus
 
     % Get value of the path length that is smaller than 99.5% of the expected paths
     cutoff_path_length = adjusted_path_mean - 2.78 * path_stdev; % p = 0.005 for single tailed test
     
-    % Assume path is 2D random walk: rmsd = <r> * sqrt(n) --> (path length / n) * sqrt(n) --> path length / sqrt(n)
-    % NOTE: behavior as n --> infinity: rmsd = sqrt(n)* mu - 2.78 * sigma, we shouldn't get stuck in an infinite loop, but this is a foolish
-    %   algorithm for large dividers and small steps. In such a case, use the limiting behavior equation instead.
-    cutoff_rmsd_estimate = cutoff_path_length / sqrt(number_points);
+%     % Assume path is 2D random walk: rmsd = <r> * sqrt(n) --> (path length / n) * sqrt(n) --> path length / sqrt(n)
+%     % NOTE: behavior as n --> infinity: rmsd = sqrt(n)* mu - 2.78 * sigma, we shouldn't get stuck in an infinite loop, but this is a foolish
+%     %   algorithm for large dividers and small steps. In such a case, use the limiting behavior equation instead. Asks for far too many
+%     %   points for this application, we have generally directed motion.
+%     adjusted_cutoff_estimate = cutoff_path_length / sqrt(number_points);
+
+    % Adjust path length by a simple ratio of the overall path length to displacement.
+    adjusted_cutoff_estimate = cutoff_path_length / path_length_to_displacement_ratio;
     
     % Stop loop if we've gone enough steps or reached the cap
-    if cutoff_rmsd_estimate > divider || number_points >= max_iterations;
+    if adjusted_cutoff_estimate > divider || number_points >= max_iterations;
         break
     else
         % Add another point
@@ -452,6 +463,7 @@ hold on
 plot(divider_points(:, 1), divider_points(:, 2), '-xb');
 scatter(current_point_set(:, 1), current_point_set(:, 2), 5, 'g');
 scatter(current_divider_point(1), current_divider_point(2), 5, 'k')
+axis equal
 hold off
 waitforbuttonpress
 end
