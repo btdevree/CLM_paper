d817940c55f4ec8ca2ce25e25bcb8c2dffe20ffd
Brian DeVree <btdevree@gmail.com>
2016-10-08 16:40:37 -0400

    image compression working

--- Common/calc_image_compression_size.m
+++ Common/calc_image_compression_size.m
@@ -1,15 +1,16 @@
 function [ file_size ] = calc_image_compression_size(image, number_bins)
 %CALC_IMAGE_COMPRESSION_SIZE Calcualte the file size of a compressed image 
 %   using the LZMA2 algorithm from p7zip.
+%
+%   Works on uint16 images for enhansed speed and accuracy of compression.
 % 
 % Inputs:
-%   image: possibly sparse array of floating-point doubles or uint16.
+%   image: possibly sparse array of floating-point or uint16 values.
 %   number_bins: number of discrete values used when writing the image 
 %       files to be compressed. Default = []; no changes to the number of
 %       values.
 % Outputs:
-%   entropy_value: floating-point double value, given in units determined
-%       by the log_base input
+%   file_size: floating-point double value, given in bytes
 
 % Set defaults
 if nargin < 2; number_bins = []; end;
@@ -26,22 +27,42 @@ if ~isempty(number_bins)
     image = image - min(image(:));
     image = image / max(image(:));
     image = image * (number_bins - 1);
-    image = round(image);
+    image = uint16(round(image));
+
+% Convert floating point images
+elseif ~isa(image, 'uint16');
+    
+    % Convert to double before manipulating
+    image = double(image);
+    image = image - min(image(:));
+    image = image / max(image(:));
+    image = image * (2.^16 - 1);
+    image = uint16(round(image));
 end
 
+% We assert that the image is now of uint16 type
+assert(isa(image, 'uint16'));
 
+% Create file to compress
+filename = tempname;
+fileID = fopen(filename, 'w');
+fwrite(fileID, image, 'uint16');
+fclose(fileID);
 
-% Calculate entropy
-if strcmp(log_base, '2')
-    entropy_values = -(pdf .* log2(pdf));
-end
-if strcmp(log_base, 'e')
-    entropy_values = -(pdf .* log(pdf));
-end
-if strcmp(log_base, '10')
-    entropy_values = -(pdf .* log10(pdf));
-end
-entropy_value = nansum(entropy_values(:));
+% Call p7zip to compress file
+command = ['p7zip ', filename];
+[status, cmdout] = system(command);
 
+% Check status, print message if not 0
+if ~status == 0
+    cmdout
+end
+
+% Check file size after compression
+file_info = dir([filename, '.7z']);
+file_size = file_info.bytes;
+
+% Delete file
+delete([filename, '.7z']);
 end
 
